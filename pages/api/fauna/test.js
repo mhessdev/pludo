f14a73f7ceb1bf2ed4d5f74ed602a0061c93391d
Marc Hes <marc@mhessdev.com>
2022-05-14 14:41:29 -0400

    made fauna folder in lib with broken up files to keep fauna functions easily finable

--- pages/api/fauna/test.js
+++ pages/api/fauna/test.js
@@ -1,3 +1,33 @@
-import { upsertDocument } from "@/lib/fauna";
+import { createIndex } from "@/lib/fauna";
 
-export default async function handler(req, res) {}
+export default async function handler(req, res) {
+    try {
+        const {
+            collection,
+            name,
+            terms,
+            values,
+            unique,
+            serialized,
+            permissions,
+            data,
+        } = req.body;
+
+        const index = await createIndex(
+            collection,
+            name,
+            terms,
+            values,
+            unique,
+            serialized,
+            permissions,
+            data
+        );
+        res.status(200).json(index);
+    } catch (err) {
+        res.status(500).json({
+            message: "Error Creating Index",
+            error: err.message,
+        });
+    }
+}
